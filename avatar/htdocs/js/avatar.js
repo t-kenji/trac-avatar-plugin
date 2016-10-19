33a6c6b1e1fac3fe697a0206e8291f75dff96e3f
t-kenji <protect.2501@gmail.com>
2016-10-19 16:30:13 +0000

    Fix tooltip undefined error when jquery ui not loaded

--- avatar/htdocs/js/avatar.js
+++ avatar/htdocs/js/avatar.js
@@ -1,9 +1,11 @@
 $(function () {
-    $('.avatar').tooltip({
-        items: '.avatar',
-        content: function () {
-            var src = $(this).attr('src');
-            return '<img class="avatar detail" src="' + src + '" />';
-        }
-    });
+    if (typeof jQuery.ui !== 'undefined') {
+        $('.avatar').tooltip({
+            items: '.avatar',
+            content: function () {
+                var src = $(this).attr('src');
+                return '<img class="avatar detail" src="' + src + '" />';
+            }
+        });
+    }
 });
